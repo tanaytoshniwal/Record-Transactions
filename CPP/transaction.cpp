8b49b0a4ed8adc3f508ae03480c98e4e7ea16bcd
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-11-01 23:26:39 +0530

    Keeping Transaction Records in CPP

---
+++ CPP/transaction.cpp
@@ -0,0 +1,50 @@
+#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
+string currdate="";
+namespace patch
+{
+    template < typename T > std::string to_string( const T& n )
+    {
+        ostringstream stm ;
+        stm << n ;
+        return stm.str() ;
+    }
+}
+string getDate(){
+	time_t t=time(0);
+    struct tm * now = localtime( & t );
+    string date=patch::to_string(now->tm_mday) +"-"+ patch::to_string(now->tm_mon+1)+"-"+patch::to_string(now->tm_year+1900);
+    return date;
+}
+int main(int argc, char *argv[]){
+	currdate=getDate();
+	ofstream file("transactions.txt",fstream::app);
+	file<<currdate<<":"<<endl;
+	int n=1;
+	do{
+		cout<<"Input Transactions?\n1:Yes\n2:No\n";
+		try{
+			cin>>n;
+			if(cin.fail())
+				throw "wrong input nigga!";
+			if(n==2){
+				cout<<"bbye";
+				break;
+			}
+		}catch(const char* msg){
+			cout<<msg;
+		}
+		cout<<"Input Transactions(in a single line):\n";
+		string str;
+		cin.clear();
+		cin.sync();
+		getline(cin ,str);
+		file<<"\t"<<str<<endl;
+	}while(n==1);
+	file.close();
+	return 0;
+}
